91a8a5a05e74dd578e9d0d1a78f1fcf53da47bcd
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-04-01 00:24:31 +0200

    0024

--- src/main/java/com/herokuapp/getadvertisment/controller/Login.java
+++ src/main/java/com/herokuapp/getadvertisment/controller/Login.java
@@ -14,13 +14,13 @@ public class Login extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp){
-        System.out.println("Get method called");
+        /*System.out.println("Get method called");
         try {
             RequestDispatcher view = req.getRequestDispatcher("login.jsp");
             view.forward(req,resp);
         }catch (Exception e){
             e.printStackTrace();
-        }
+        }*/
     }
 
     @Override
